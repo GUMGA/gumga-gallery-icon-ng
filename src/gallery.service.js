b85c287064b465d26691cd70e03a84d924ad3946
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-04-17 14:11:54 -0300

    1.0.4

--- src/gallery.service.js
+++ src/gallery.service.js
@@ -7,13 +7,13 @@
         {
           name: 'Font Awesome',
           prefix: 'fa',
-          url: 'http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+          url: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
           icons: []
         },
         {
           name: 'Material Icons',
           prefix: 'zmdi',
-          url: 'http://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css',
+          url: 'https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css',
           icons: []
         }
       ]
